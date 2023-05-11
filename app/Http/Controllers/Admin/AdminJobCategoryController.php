ae6fdedf26f2ac96e000c7cd6a9ecea7818f35e2
Hoang Nhat <hoangminhnhat159@gmail.com>
2023-05-11 14:23:35 +0700

    fourth commit

--- app/Http/Controllers/Admin/AdminJobCategoryController.php
+++ app/Http/Controllers/Admin/AdminJobCategoryController.php
@@ -20,5 +20,31 @@ class AdminJobCategoryController extends Controller
             'name' => 'required',
             'icon' => 'required'
         ]);
+        
+        $obj = new JobCategory();
+        $obj->name = $request->name;
+        $obj->icon = $request->icon;
+        $obj->save();
+        return redirect()->route('admin_job_category')->with('success', 'Data is saved successfully!');
+    }
+    public function edit($id) {
+        $job_category_single = JobCategory::where('id', $id)->first();
+        return view('admin.job_category_edit', compact('job_category_single'));             
+    }
+    public function update(Request $request, $id) {
+        $obj = JobCategory::where('id', $id)->first();
+        $request->validate([
+            'name' => 'required',
+            'icon' => 'required'
+        ]);
+        
+        $obj->name = $request->name;
+        $obj->icon = $request->icon;
+        $obj->save();
+        return redirect()->route('admin_job_category')->with('success', 'Data is updated successfully!');
+    }
+    public function delete($id) {
+        JobCategory::where('id', $id)->delete();
+        return redirect()->route('admin_job_category')->with('success', 'Data is deleted successfully!');
     }
 }
--- resources/views/admin/job_category.blade.php
+++ resources/views/admin/job_category.blade.php
@@ -21,6 +21,7 @@
                                     <th>SL</th>
                                     <th>Category Name</th>
                                     <th>Category Icon</th>
+                                    <th>Icon Preview</th>
                                     <th>Action</th>
                                 </tr>
                                 </thead>
@@ -30,8 +31,10 @@
                                         <td>{{ $loop->iteration }}</td>
                                         <td>{{ $item->name }}</td>
                                         <td>{{ $item->icon }}</td>
-                                            <a href="" class="btn btn-primary btn-sm">Edit</a>
-                                            <a href="" class="btn btn-danger" onClick="return confirm('Are you sure?');">Delete</a>
+                                        <td><i class="{{ $item->icon }}"></i></td>
+                                        <td class="pt_10 pb_10">
+                                            <a href="{{ route('admin_job_category_edit', $item->id) }}" class="btn btn-primary btn-sm">Edit</a>
+                                            <a href="{{ route('admin_job_category_delete', $item->id) }}" class="btn btn-danger btn-sm" onClick="return confirm('Are you sure?');">Delete</a>
                                         </td>                                    
                                     </tr> 
                                     @endforeach                              
