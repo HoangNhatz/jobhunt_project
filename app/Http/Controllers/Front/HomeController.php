02f9269f5c9c6311328120240823ac7003deb812
Hoang Nhat <hoangminhnhat159@gmail.com>
2023-05-17 08:42:18 +0700

    fifth commit

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\PageHomeItem;
+use App\Models\JobCategory;
 
 class HomeController extends Controller
 {
     public function index() {
         $page_home_data = PageHomeItem::where('id',1)->first();
-        return view('front.home', compact('page_home_data'));
+        $job_categories = JobCategory::orderBy('name', 'asc')->take(9)->get();
+        return view('front.home', compact('page_home_data', 'job_categories'));
     }
 }
